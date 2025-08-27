a11fdc827aa9209e9d4243740e719e5d8535d592
Pratt33 <pratikshirsath2223_ec@sanjivanicoe.org.in>
2025-08-28 00:39:01 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+	if request.method == 'POST':
+		user_input = request.form.get('user_input', '')
+		return f"You entered: {user_input}"
+	return '''
+		<form method="post">
+			<input type="text" name="user_input" placeholder="Enter something">
+			<input type="submit" value="Submit">
+		</form>
+	'''
+
+if __name__ == '__main__':
+	app.run(debug=True)
